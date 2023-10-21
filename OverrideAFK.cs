b026c22e2e4c0c5363df2379b004c76ba72228ad
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2023-10-21 12:42:22 +0100

    Use CS input timer module instead of custom stuff.

--- OverrideAFK.cs
+++ OverrideAFK.cs
@@ -1,35 +1,15 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.UI;
-using System.Runtime.InteropServices;
 
 namespace visland;
 
-[StructLayout(LayoutKind.Explicit, Size = 0x4F8)]
-public unsafe struct AfkModule
-{
-    [FieldOffset(0x10)] public float ElapsedForAfkMessage;
-    [FieldOffset(0x14)] public float ElapsedForKick;
-    [FieldOffset(0x18)] public float ElapsedUnk1;
-    [FieldOffset(0x1C)] public float ElapsedUnk2;
-}
-
 public unsafe class OverrideAFK
 {
-    private AfkModule* _module;
-
-    public OverrideAFK()
-    {
-        var uiModule = UIModule.Instance();
-        var uiModuleVtbl = (void**)uiModule->VTable;
-        var getAfkModule = (delegate* unmanaged[Stdcall]<UIModule*, AfkModule*>)uiModuleVtbl[55];
-        _module = getAfkModule(uiModule);
-        Service.Log.Info($"Module: {(nint)_module:X}");
-    }
-
     public void ResetTimers()
     {
-        _module->ElapsedForAfkMessage = 0;
-        _module->ElapsedForKick = 0;
-        _module->ElapsedUnk1 = 0;
-        _module->ElapsedUnk2 = 0;
+        var module = UIModule.Instance()->GetInputTimerModule();
+        module->AfkTimer = 0;
+        module->ContentInputTimer = 0;
+        module->InputTimer = 0;
+        module->Unk1C = 0;
     }
 }
