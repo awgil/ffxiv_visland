d3f2e82f37be8a45652b29e67f18506b67f86a3b
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-01-09 15:22:21 +0000

    enforce anglo supremacy

--- ffxiv_visland/Plugin.cs
+++ ffxiv_visland/Plugin.cs
@@ -3,9 +3,11 @@ using Dalamud.Game.Command;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
 using ECommons;
+using ECommons.DalamudServices;
 using ECommons.Reflection;
 using ImGuiNET;
 using System;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Numerics;
@@ -161,7 +163,7 @@ public sealed class Plugin : IDalamudPlugin
     {
         var originActor = relativeToPlayer ? Service.ClientState.LocalPlayer : null;
         var origin = originActor?.Position ?? new();
-        var offset = new Vector3(float.Parse(args[1]), float.Parse(args[2]), float.Parse(args[3]));
+        var offset = new Vector3(float.Parse(args[1], new CultureInfo("en-US")), float.Parse(args[2], new CultureInfo("en-US")), float.Parse(args[3], new CultureInfo("en-US")));
         var route = new GatherRouteDB.Route { Name = "Temporary", Waypoints = new() };
         route.Waypoints.Add(new() { Position = origin + offset, Radius = 0.5f, InteractWithName = "", InteractWithOID = 0 });
         _wndGather.Exec.Start(route, 0, false, false);
